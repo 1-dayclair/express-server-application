41362d3021a79f6af04e9440a50c84cc51f9aa96
super-time1 <168475235+super-time1@users.noreply.github.com>
2024-07-15 06:51:16 -0400

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 
 
 app.set("view engine", "ejs");
-app.use("/public", express.static("public"))
+app.use("/public", express.static("public"));
+app.use("/public/media", express.static("public"));
 
 const bodyParser = require("body-parser");
 
