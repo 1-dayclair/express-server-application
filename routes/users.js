a469e4971cbfdb1e0d140edb5d864a987327711e
super-time1 <168475235+super-time1@users.noreply.github.com>
2024-07-29 00:58:49 -0400

    *update

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,6 @@ const users = require("../data/users");
 const router = express.Router();
 
 
-// router
-//     .route("/")
-//     .get((req, res) => {
-//         res.json(users); 
-//     });
-
 router 
     .route("/person")
     .get((req, res) => {
@@ -33,16 +27,20 @@ router
         res.send(newUser);
         console.log(`Brand new user!`);
     });
-// router 
-//     .route("/person")
-//     .get((req, res) => {
-//         console.log(req.query);
-//         return res.json({
-//             message: "This user is an actual person on Circular!", 
-//             name: req.query.name,
-//             age: req.query.age,    
-//         });
-//     });
+
+router 
+    .route("/edit/:id")
+    .put(async (req, res) => {
+        try { 
+            const update = (req.params.id, req.body)
+
+            console.log("Data Update!",{update});
+            res.json({update});
+        } catch (error) {
+            res.status(500).json({message:error.message});
+        }
+    });
+
 router 
     .route("/:id")
     .delete((req, res, next) => { 
